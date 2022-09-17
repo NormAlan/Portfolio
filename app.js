e2504d1d376ab0fe030948436db81da15181b8fd
NormAlan <guardenwolf@gmail.com>
2022-09-17 19:23:11 +0300

    some updates

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const sections = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('.controlls');
+/*const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
 
@@ -38,4 +38,18 @@ allSections.addEventListener('click', (e) => {
   })
 }
 
-PageTransition();
+PageTransition(); */
+
+(function(){
+    [...document.querySelectorAll(".control")].forEach(button =>{
+        button.addEventListener("click", function(){
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+}) ();
